0c8cc70a085c600262131b8f478696b217b2f511
Meng Hak <meng.hak@xcentium.com>
2023-10-27 19:27:29 -0700

    remove old starter

--- src/sxastarter/src/components/Feature/Article.tsx
+++
@@ -1,8 +0,0 @@
-const Article = (): JSX.Element => (
-    <section>
-        <h1>My first article</h1>
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-    </section>
-);
- 
-export default Article
