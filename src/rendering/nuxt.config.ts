e61dc8b01e316327c50d1ddb7544287cc30e606e
Meng Hak <meng.hak@xcentium.com>
2023-11-06 20:05:03 -0800

    clean up

--- src/rendering/nuxt.config.ts
+++ src/rendering/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src/',
-  // components: ['~/src/components'],
   devtools: { enabled: true },
   alias: {
     "@/": "src/",
